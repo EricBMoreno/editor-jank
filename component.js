b69c04485d9b50de3785fd4973e393dfbcacaf87
Eric Moreno <160285215+EricBMoreno@users.noreply.github.com>
2026-03-25 00:35:01 -0700

    Update CSS selector for slotted buttons

--- component.js
+++ component.js
@@ -255,7 +255,8 @@ class textEditor extends HTMLElement {
             --command-icon: url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" fill="%23000000" width="25px" height="25px" viewBox="0 0 24 24"><path d="M21,4H3A1,1,0,0,0,2,5V19a1,1,0,0,0,1,1H21a1,1,0,0,0,1-1V5A1,1,0,0,0,21,4ZM7,7A2,2,0,1,1,5,9,2,2,0,0,1,7,7ZM20,18H4V16.333L8,13l2.857,2.143L16,10l4,5.333Z"/></svg>');
         }
 
-        ::slotted(button:not([command])) {
+        ::slotted(button:not([command])),
+        ::slotted(button[command=""]) {
             display: none;
         }
 
